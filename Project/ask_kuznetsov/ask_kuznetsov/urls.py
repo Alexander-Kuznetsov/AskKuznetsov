57fbb980ba1407b9a2f49caa6b7091211230bdbe
Alexander Kuznetsov <seelk73@gmail.com>
2018-01-20 22:05:52 +0300

    hw3

--- Project/ask_kuznetsov/ask_kuznetsov/urls.py
+++ Project/ask_kuznetsov/ask_kuznetsov/urls.py
@@ -1,8 +1,17 @@
-from django.conf.urls import url, include
 from django.contrib import admin
-
+from django.conf.urls import url, include
+from views import *
+from views import index_view
+  
 urlpatterns = [
     url(r'question/', include('questions.urls')),
-
     url(r'^admin/', admin.site.urls),
+    url(r'^$', index_view, name='index'),
+    url(r'^index', index_view, name='index'),
+    url(r'^signup', signup_view, name='singup'),
+    url(r'^ask', ask_view, name='ask'),
+    url(r'^answer', answer_view, name='answer'),
+    url(r'^login', login_view, name='login'),
+    url(r'^settings', settings_view, name='settings'),
+    url(r'^tag', tag_view, name='tag')
 ]
--- Project/ask_kuznetsov/ask_kuznetsov/views.py
+++ Project/ask_kuznetsov/ask_kuznetsov/views.py
@@ -1,9 +1,28 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
+from django.shortcuts import render_to_response, redirect
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
+from django.shortcuts import get_object_or_404
+from django.contrib import auth
 
-from django.shortcuts import render
+from django.contrib.auth.models import User
 
-from django.views.generic import TemplateView
 
-class AboutView(TemplateView):
-    template_name = "about.html"
+def index_view(request, *args, **kwargs):
+    return render_to_response('index.html', kwargs)
+
+def signup_view(request, *args, **kwargs):
+    return render_to_response('signup.html', kwargs)
+
+def ask_view(request, *args, **kwargs):
+    return render_to_response('ask.html', kwargs)
+
+def answer_view(request, *args, **kwargs):
+    return render_to_response('answer.html', kwargs)
+
+def login_view(request, *args, **kwargs):
+    return render_to_response('login.html', kwargs)
+
+def settings_view(request, *args, **kwargs):
+    return render_to_response('settings.html', kwargs)
+
+def tag_view(request, *args, **kwargs):
+    return render_to_response('tag.html', kwargs)
