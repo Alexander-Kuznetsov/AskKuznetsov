8f31a0e3d744bee83de82c11a190ebb418ce4661
Alexander Kuznetsov <seelk73@gmail.com>
2018-01-22 00:08:08 +0300

    load db
    load for index

--- ask_kuznetsov/ask_kuznetsov/views.py
+++ ask_kuznetsov/ask_kuznetsov/views.py
@@ -1,13 +1,15 @@
-from django.shortcuts import render_to_response, redirect
+from django.shortcuts import render_to_response, redirect, render
 from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from django.shortcuts import get_object_or_404
 from django.contrib import auth
 
+from ask_kuznetsov.models import *
+
 from django.contrib.auth.models import User
 
-
 def index_view(request, *args, **kwargs):
-    return render_to_response('index.html', kwargs)
+    articles = Question.objects.all()
+    return pagination(request, 'index.html', articles,'articles', 10, *args, **kwargs)
 
 def signup_view(request, *args, **kwargs):
     return render_to_response('signup.html', kwargs)
@@ -26,3 +28,19 @@ def settings_view(request, *args, **kwargs):
 
 def tag_view(request, *args, **kwargs):
     return render_to_response('tag.html', kwargs)
+
+def pagination(request, html_page, objects, object_name, objects_count, *args, **kwargs):
+    paginator = Paginator(objects, objects_count)
+    page = request.GET.get('page')
+
+    try:
+        objects = paginator.page(page)
+    except PageNotAnInteger:
+        objects = paginator.page(1)
+    except EmptyPage:
+        objects = paginator.page(paginator.num_pages)
+
+    kwargs[object_name] = objects
+    kwargs['pagination_list'] = objects
+
+    return render(request,html_page, kwargs)
